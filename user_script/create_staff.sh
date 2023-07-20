62ceebebcc14d9d7076da5a392c2bd46810c21ee
Dominic <dominic.anyanna@gmail.com>
2023-07-20 07:58:28 +0100

    script

--- user_script/create_staff.sh
+++ user_script/create_staff.sh
@@ -5,4 +5,4 @@ csv_file="users.csv"
 while IFS=',' read -r  password username email
 do
     tutor local do createuser --staff -p "$password" "$username" "$email"
-done < "$csv_file"
+done < <(tail -n +2 "$csv_file")
