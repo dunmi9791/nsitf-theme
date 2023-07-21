87b8186bc86472314ce4d3c380f57fc4bbfefabc
Dominic <dominic.anyanna@gmail.com>
2023-07-21 11:14:37 +0100

    support users

---
+++ user_script/create_learner.py
@@ -0,0 +1,11 @@
+import csv
+import subprocess
+
+csv_file = "users.csv"
+
+with open(csv_file, "r") as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip header line
+    for row in reader:
+        password, username, email = row
+        subprocess.run(["tutor", "local", "do", "createuser", "-p", password, username, email])
